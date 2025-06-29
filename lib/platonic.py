fbcd314534928e0c45ea705c8ac6a40fbc67cdc4
Anderson Winkler <andersonwinkler@gmail.com>
2025-06-29 15:00:29 -0500

    smoothing seems ok

--- lib/platonic.py
+++ lib/platonic.py
@@ -571,38 +571,3 @@ def dpf2dpv(dpf, fac, facu=None, pycno=False, fsmode=True):
             # Average by the number of faces that meet at that vertex
             dpv /= cnt
     return dpv
-
-def make_fine_fsaverage(outdir=None):
-    '''
-    Make fsaverage surfaces (icosahedrons recursively subdivided) and save them
-    in the module's tree structure. These are "finer" than the original shipped
-    with FreeSurfer in that vertex coordinates have more decimal places, and
-    recursions span the range 0-9, as opposed to 3-7.
-    Instead of lh and rh (which are identical), saves as "xh".
-    '''
-    import os
-    from . import io
-    
-    # Find FreeSurfer
-    fshome = os.getenv('FREESURFER_HOME')
-    if fshome is None:
-        raise ValueError('Error: FREESURFER_HOME variable not set. Stopping.')
-    
-    # Where to save
-    if outdir is not None:
-        outdir = os.path.join(os.path.dirname(__file__), 'etc', 'fsaverage')
-    
-    # Read lh.sphere.reg so we can get the coordinate transformation (just in case)
-    # The lh.sphere.reg is binarily identical to rh.sphere.reg, so just one of these is sufficient
-    _, _, info, _ = io.read_surf(os.path.join(fshome,'subjects','fsaverage','surf','lh.sphere.reg'))
-    
-    # Create and save fsaverage0 (no subdivision)
-    print('Working on subdivision 0')
-    vtx, fac = icosahedron(meas='cr', value=100, fsmode=True)
-    io.write_surf(os.path.join(outdir, 'xh.fsaverage0.fine.sphere.reg'), vtx, fac, info)
-    
-    # Recursively subdivide and save
-    for n in range(1,10):
-        print(f'Working on subdivision {n}')
-        vtx, fac = icoup(vtx, fac, 1, fsmode=True)
-        io.write_surf(os.path.join(outdir, f'xh.fsaverage{n}.fine.sphere.reg'), vtx, fac, info)
